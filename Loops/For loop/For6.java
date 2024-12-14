f31c3c042e912c047333069edb1fe48425a0399e
jeevan <jeevanpareek1234@gmail.com>
2024-12-14 20:02:30 +0530

    Sum of even numbers

---
+++ Loops/For loop/For6.java
@@ -0,0 +1,12 @@
+public class For6 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int a = 1; a<=100; a++){
+
+            sum = sum+a;
+        }
+        System.out.println(sum+" ");
+        
+    }
+    
+}
