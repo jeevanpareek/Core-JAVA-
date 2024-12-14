c19fcc2ad1d1071904d658860461d425c5d73144
jeevan <jeevanpareek1234@gmail.com>
2024-12-14 14:08:54 +0530

    Table of a number by user input

---
+++ Loops/While Loop/B14.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class B14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int a = sc.nextInt();
+
+        int table = 1
+        ;
+
+        while(table<=10){
+            System.out.println(table*a);
+            table++;
+        }
+    }
+    
+}
