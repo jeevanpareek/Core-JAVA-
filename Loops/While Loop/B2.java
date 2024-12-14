f5f9e51c8066fda5fbc403e4fef7d093b20c1938
jeevan <jeevanpareek1234@gmail.com>
2024-12-14 11:39:43 +0530

    new commit

---
+++ Loops/While Loop/B2.java
@@ -0,0 +1,14 @@
+//import java.util.Scanner;
+
+public class B2 {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+
+        int a = 100;
+        while(a>=1){
+            System.out.print(a+" ");
+            a--;
+        }
+    }
+    
+}
