c679461d8078354508db4328f99ce110073fdd71
jeevan <jeevanpareek1234@gmail.com>
2024-12-14 14:28:01 +0530

    Table of 2 (Reversed)

---
+++ Loops/While Loop/B17.java
@@ -0,0 +1,12 @@
+public class B17 {
+    public static void main(String[] args) {
+        int a = 2;
+        int table = 10;
+
+        while(table>=1){
+            System.out.println(table*a);
+            table--;
+        }
+    }
+    
+}
