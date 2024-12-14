7e14d14aae87e3ec4803ce4895ef7386cb8ad9d4
jeevan <jeevanpareek1234@gmail.com>
2024-12-14 11:48:19 +0530

    even numbers between 100 to 1

---
+++ Loops/While Loop/B.java
@@ -0,0 +1,14 @@
+public class B {
+    public static void main(String[] args) {
+
+        int a = 100;
+        while(a>=1){
+            if(a%2==0){
+                
+                System.out.print(a+" ");
+            }
+            a--;
+        }
+    }
+    
+}
