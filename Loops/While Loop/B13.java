b0dc70037af885eda8e32eee4f16dd0f3421c3c0
jeevan <jeevanpareek1234@gmail.com>
2024-12-14 14:06:08 +0530

    Table of 2

---
+++ Loops/While Loop/B13.java
@@ -0,0 +1,13 @@
+public class B13 {
+    public static void main(String[] args) {
+        int a = 2;
+        int table = 1
+        ;
+
+        while(table<=10){
+            System.out.println(table*a);
+            table++;
+        }
+    }
+    
+}
