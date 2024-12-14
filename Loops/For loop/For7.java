2e0809bf49a717fc09ba26024a2975e0d6f1e4af
jeevan <jeevanpareek1234@gmail.com>
2024-12-14 20:06:41 +0530

    Sum of odd numbers

---
+++ Loops/For loop/For7.java
@@ -0,0 +1,15 @@
+public class For7 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int a = 1; a<=100; a++){
+            if(a%2!=0){
+
+                sum = sum+a;
+            }
+
+        }
+        System.out.println(sum+" ");
+        
+    }
+    
+}
