8b53f6e668be78e096968d10727a8c727be11238
jeevan <jeevanpareek1234@gmail.com>
2024-12-14 20:05:34 +0530

    Sum of even numbers

--- Loops/For loop/For6.java
+++ Loops/For loop/For6.java
@@ -2,8 +2,11 @@ public class For6 {
     public static void main(String[] args) {
         int sum = 0;
         for(int a = 1; a<=100; a++){
+            if(a%2==0){
 
-            sum = sum+a;
+                sum = sum+a;
+            }
+
         }
         System.out.println(sum+" ");
         
