474decf426a0f8fbb1fef1fb45c1041d58eaa89d
jeevan <jeevanpareek1234@gmail.com>
2024-12-14 12:07:36 +0530

    Even Numbers between 50 to 1000

---
+++ Loops/While Loop/B8.java
@@ -0,0 +1,12 @@
+public class B8 {
+    public static void main(String[] args) {
+        int a = 50;
+        while(a<=1000){
+            if(a%2==0){
+                System.out.print(a+" ");
+            }
+            a++;
+        }
+    }
+    
+}
