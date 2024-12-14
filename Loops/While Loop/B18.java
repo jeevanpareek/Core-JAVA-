6e55b555c644ac9b1fac6f02d5983f3990d3b1ec
jeevan <jeevanpareek1234@gmail.com>
2024-12-14 14:32:17 +0530

    Table of 2 (Complete format)

---
+++ Loops/While Loop/B18.java
@@ -0,0 +1,13 @@
+public class B18 {
+    public static void main(String[] args) {
+        int a = 2;
+        int table = 1
+        ;
+
+        while(table<=10){
+            System.out.println(+a+"x"+table+"="+table*a);
+            table++;
+        }
+    }
+    
+}
