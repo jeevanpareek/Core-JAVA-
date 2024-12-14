019b0475564aa5a1136c0d9375fb4a47254f6c2e
jeevan <jeevanpareek1234@gmail.com>
2024-12-14 13:55:42 +0530

    Sum of numbers from 1 to 10

---
+++ Loops/While Loop/B12.java
@@ -0,0 +1,14 @@
+public class B12 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int a = 1;
+        
+        while(a<=10){
+            sum = sum+a;
+            a++;
+
+        }
+        System.out.println("The sum of numbers from 1 to 10 is " +sum);
+    }
+    
+}
