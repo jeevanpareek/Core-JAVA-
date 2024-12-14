5e459fe6899be51d72b6c54dfc78f453eb8161b0
jeevan <jeevanpareek1234@gmail.com>
2024-12-14 17:55:31 +0530

    Sum of natural numbers from 1 to 10

---
+++ Loops/For loop/For2.java
@@ -0,0 +1,10 @@
+public class For2{
+    public static void main(String[] args) {
+        int sum = 0;
+        
+        for(int a = 1; a<=10; a++){
+            sum = sum+a;
+        }
+        System.out.print(sum+" ");
+    }
+}
