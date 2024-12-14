f14883ed6ec19a341fdf1ad5228e150b375bfdda
jeevan <jeevanpareek1234@gmail.com>
2024-12-14 14:22:14 +0530

    Print numbers which are divisible by 5 &11

---
+++ Loops/While Loop/B16.java
@@ -0,0 +1,16 @@
+public class B16{
+
+    public static void main(String[] args) {
+        int a = 1;
+        while(a<=100){
+            if(a%5==0  && a%11==0){
+
+                System.out.print(a+" ");
+            }
+            a++; 
+        }
+
+    }
+    
+}
+
