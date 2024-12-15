dd2bca7d2e3595d48b5178022d37e8e9f03cedc1
jeevan <jeevanpareek1234@gmail.com>
2024-12-15 07:35:27 +0530

    Table of 2 (Reversed)

---
+++ Loops/For loop/For13.java
@@ -0,0 +1,9 @@
+public class For13 {
+    public static void main(String[] args) {
+        int a = 2;
+        for(int i=1;i<=10;i++ ){
+            System.out.println(+a+" x "+i+" = "+a*i);
+        }
+    }
+    
+}
