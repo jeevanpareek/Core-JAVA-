adc44b8c96cf8ba2f46935a60908d9a7b6b5435c
jeevan <jeevanpareek1234@gmail.com>
2024-12-15 07:36:40 +0530

    Table of 2 (Complete Format)

--- Loops/For loop/For13.java
+++ Loops/For loop/For13.java
@@ -1,7 +1,7 @@
 public class For13 {
     public static void main(String[] args) {
         int a = 2;
-        for(int i=1;i<=11;i++ ){
+        for(int i=1;i<=10;i++ ){
             System.out.println(+a+" x "+i+" = "+a*i);
         }
     }
