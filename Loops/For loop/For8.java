8a197df84c19224e03e8f9b9987bc4bd23201f9f
jeevan <jeevanpareek1234@gmail.com>
2024-12-15 07:13:47 +0530

    Tables from 2 to 10

---
+++ Loops/For loop/For8.java
@@ -0,0 +1,12 @@
+public class For8 {
+    public static void main(String[] args) {
+        for(int a=2;a<=10;a++){
+            System.out.println("Table of "+a+ " is :");
+            for(int b=1;b<=10;b++){
+                System.out.println(a*b+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
