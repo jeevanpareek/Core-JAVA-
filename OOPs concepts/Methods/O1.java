1fd70be6159c4eefcd47139e965571bb263d4b7e
jeevan <jeevanpareek1234@gmail.com>
2024-12-18 14:03:12 +0530

    Print hello world using methods

---
+++ OOPs concepts/Methods/O1.java
@@ -0,0 +1,12 @@
+class A{
+    void show(){
+        System.out.println("Hello World");
+    }
+}
+
+    public class O1{
+        public static void main(String[] args) {
+            A p = new A();
+            p.show();
+        }
+    }
