a135e41f3d9824ad567516c8a4dcbf4a137ee8b2
jeevan <jeevanpareek1234@gmail.com>
2024-12-18 15:28:01 +0530

    print a number using methods

---
+++ OOPs concepts/Methods/No Return & Argument/N1.java
@@ -0,0 +1,12 @@
+class A{
+    void show(int a){
+        System.out.println(a);
+    }
+}
+public class N1{
+    public static void main(String[] args) {
+        A p = new A();
+        p.show(5);
+        
+    }
+}
