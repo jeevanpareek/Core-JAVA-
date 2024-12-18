9e706598c46a08c16abebf343af713ed71106439
jeevan <jeevanpareek1234@gmail.com>
2024-12-18 15:20:38 +0530

    Multiple methods in a single class

---
+++ OOPs concepts/Methods/No Return & No Argument/O2.java
@@ -0,0 +1,16 @@
+class A{
+    void show(){
+        System.out.println("Hello Friends !!");
+ 
+    }
+    void show1(){
+        System.out.println("My name is Jeevan Pareek");
+    }
+}
+public class O2{
+    public static void main(String[] args) {
+        A p = new A();
+        p.show();
+        p.show1();
+    }
+}
