aeca771d9e8f2bfe09747a6cd97651a9100056a4
jeevan <jeevanpareek1234@gmail.com>
2024-12-19 07:34:20 +0530

    Print a string

---
+++ OOPs concepts/Methods/Return & No Argument/R3.java
@@ -0,0 +1,12 @@
+class A{
+    String a(){
+        String name = "jeevan";
+        return name;
+    }
+}
+public class R3 {
+    public static void main(String[] args) {
+        A p = new A();
+        System.out.println(p.a());
+    }
+}
