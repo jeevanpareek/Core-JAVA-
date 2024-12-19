4ad67b2d542d26ce2721852a290e4f7ba5dbcdd7
jeevan <jeevanpareek1234@gmail.com>
2024-12-19 15:12:25 +0530

    String

---
+++ OOPs concepts/Methods/Return & Arguments/R2.java
@@ -0,0 +1,11 @@
+class A{
+  String a(String s){
+    return s;
+  }  
+}
+public class R2 {
+    public static void main(String[] args) {
+        A p = new A();
+        System.out.println(p.a("jeevan"));
+    }
+}
