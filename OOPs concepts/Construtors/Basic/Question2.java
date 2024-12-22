03eccbc4f8f2a70a6ced58248feff5d12d38f692
jeevan <jeevanpareek1234@gmail.com>
2024-12-22 07:55:38 +0530

    Default Constructors

---
+++ OOPs concepts/Construtors/Basic/Question2.java
@@ -0,0 +1,13 @@
+class A{
+    A(){
+        String name = "Unknown";
+        int age = 0; 
+
+        System.out.println(name+ " " +age);
+    }
+}
+public class Question2 {
+    public static void main(String[] args) {
+        new A();
+    }
+}
