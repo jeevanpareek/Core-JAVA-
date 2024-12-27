93a9171a64cd8187147da24248b278ae33f9ecbe
jeevan <jeevanpareek1234@gmail.com>
2024-12-27 14:15:59 +0530

    Constructor Overloading in Polymorphism

---
+++ OOPs concepts/Polymorphism/I2.java
@@ -0,0 +1,19 @@
+class A{
+    A(){
+        System.out.println("hello");
+    }
+    A(){
+        System.out.println("you");
+    }
+    A(){
+        System.out.println("are");
+    }
+    A(){
+        System.out.println("buffalo");
+    }
+}
+public class I2 {
+    public static void main(String[] args) {
+        new A();
+    }
+}
