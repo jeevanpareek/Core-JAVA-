451ce46029e6a365b62fc46be2eba15aad2b280f
jeevan <jeevanpareek1234@gmail.com>
2025-01-08 17:21:28 +0530

    Reverse Alphabet Triangle Pattern

---
+++ Patterns/Pattern10.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Pattern10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n : ");
+        int b = sc.nextInt();
+        int n = b;
+
+        for(int i=1;i<=b;i++){
+            for(int j=n;j>=1;j--){
+                System.out.print((char)(j+64)+" ");
+            }
+            n--;
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
