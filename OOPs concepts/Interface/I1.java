cda34f687da42ceda5204283765db8d41f7c3b23
jeevan <jeevanpareek1234@gmail.com>
2025-01-09 15:30:47 +0530

    Interface

---
+++ OOPs concepts/Interface/I1.java
@@ -0,0 +1,40 @@
+// interface : 
+// 	method1 :no return and no argument 
+// 	method2:no return and  argument 
+// 	method3: return and no argument 
+// 	method4 : return and  argument 
+interface A{
+    void show();
+    void show1(int a);
+     int show2();
+     int show3(int b);
+}
+class B implements A{
+    
+    public void show(){
+        System.out.println("Hello");
+    }
+    public void show1(int a){
+        System.out.println(a);
+    }
+    public int show2(){
+        System.out.println("Hii");
+        
+        return 0;
+    }
+    public int show3(int b){
+        System.out.println(b);
+
+        return b;
+    }
+
+}
+public class I1{
+    public static void main(String[] args) {
+        B p = new B();
+        p.show();
+        p.show1(20);
+         p.show2();
+         p.show3(45);
+    }
+}
