48808c417db4d88699e63cdc354974bf6725060e
jeevan <jeevanpareek1234@gmail.com>
2025-01-09 16:09:49 +0530

    Multi-level Inheritence in Interface

---
+++ OOPs concepts/Interface/I2.java
@@ -0,0 +1,67 @@
+// multiple inheritance
+
+// interface : 
+// 	method1 :no return and no argument 
+
+// interface : 
+// 	method2:no return and  argument 
+
+// interface : 
+// 	method3: return and no argument 
+
+// interface : 
+// 	method4 : return and  argument 
+
+// another class access 
+interface A{
+    void show();
+} 
+interface B{
+    int show1(int a);
+} 
+interface C{
+    int show2();
+} 
+interface D{
+    void show3(int a);
+} 
+
+class E{
+    public void show(){
+        System.out.println("Hello");
+    }
+}
+class F{
+    public int show(int a){
+        System.out.println(a);
+
+        return a;
+    }
+}
+class G{
+    public int show(){
+        System.out.println("Hello");
+
+        return 0;
+    }
+}
+class H{
+    public void show(int b){
+        System.out.println(b);
+    }
+}
+public class I2 {
+    public static void main(String[] args) {
+        E p = new E();
+        p.show();
+
+        F q = new F();
+        q.show(20);
+
+        G w = new G();
+        w.show();
+
+        H e = new H();
+        e.show(45);
+    }
+}
