079b3b973d2b6de41e94e00487e516b83efa9256
jeevan <jeevanpareek1234@gmail.com>
2025-01-10 11:02:58 +0530

    Reverse Alphabet Triangle Pattern

--- Patterns/Pattern10.java
+++ Patterns/Pattern10.java
@@ -6,13 +6,13 @@ public class Pattern10 {
 
         System.out.print("Enter n : ");
         int b = sc.nextInt();
-        int n = b;
+          int n = b;
 
         for(int i=1;i<=b;i++){
             for(int j=n;j>=1;j--){
                 System.out.print((char)(j+64)+" ");
             }
-            n--;
+              n--;
             System.out.println();
         }
         sc.close();
