0361b6c04829ca619afadd85155b7d717a63c125
jeevan <jeevanpareek1234@gmail.com>
2025-02-03 17:11:00 +0530

    Static block in Main class

---
+++ OOPs concepts/Static (keyword)/Q9.java
@@ -0,0 +1,24 @@
+class A{
+    static{
+        System.out.println("Hello");
+    }
+    static{
+        System.out.println("Hii");
+    }
+    static{
+        System.out.println("Jeevan");
+    }
+    static{
+        System.out.println("Pareek");
+    }
+}
+public class Q9 {
+    static{
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        System.out.println("JAVA Full Stack");
+
+        new A();
+    }
+}
