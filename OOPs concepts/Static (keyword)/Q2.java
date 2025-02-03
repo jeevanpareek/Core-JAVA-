7e5f9325fe031d40310bed664747873a15f05549
jeevan <jeevanpareek1234@gmail.com>
2025-02-03 15:10:19 +0530

    Accessing Static & non static class in a normal class

---
+++ OOPs concepts/Static (keyword)/Q2.java
@@ -0,0 +1,14 @@
+// class A{
+//     static class B{
+//         static A show(){
+//             System.out.println("Hello");
+
+//             return A;
+//         }
+//     }
+// }
+// public class Q2 {
+//    public static void main(String[] args) {
+//     A.B.show();
+//    } 
+// }
