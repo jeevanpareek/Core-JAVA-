9f962d8b3db599b68f870b62b957ebbdbaa62ad5
jeevan <jeevanpareek1234@gmail.com>
2025-02-04 12:20:26 +0530

    Basic Arithemetic operations using Aggregation

---
+++ OOPs concepts/Aggregation/Question 1/P4.java
@@ -0,0 +1,7 @@
+public class P4 {
+    public static void main(String[] args) {
+        new Add();
+        new Sub();
+        new Multiply();
+    }
+}
---
+++ OOPs concepts/Aggregation/Question 1/Sub.java
@@ -0,0 +1,10 @@
+class Sub{
+    Sub(){
+        int a = 5;
+        int b = 10;
+
+        int c = a-b;
+
+        System.out.println(c);
+    }
+}
