53e50d8770b3c7cc6f8e05b2a45322bdc6c7ef67
jeevan <jeevanpareek1234@gmail.com>
2025-02-04 13:42:07 +0530

    Parameterised constructor in Aggregation

---
+++ OOPs concepts/Aggregation/Question2/P1.java
@@ -0,0 +1,7 @@
+class P1 {
+    P1(int a, int b){
+        int c = a*b;
+
+        System.out.println(c);
+    }
+}
---
+++ OOPs concepts/Aggregation/Question2/P2.java
@@ -0,0 +1,5 @@
+public class P2 {
+    public static void main(String[] args) {
+        new P1(20,2);
+    }
+}
