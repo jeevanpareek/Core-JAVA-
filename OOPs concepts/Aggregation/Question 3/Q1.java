9e2b6ca484086d6ab340ad129ee428f20b8b5450
jeevan <jeevanpareek1234@gmail.com>
2025-02-04 13:49:53 +0530

    Methods in Aggregation

---
+++ OOPs concepts/Aggregation/Question 3/Q1.java
@@ -0,0 +1,23 @@
+class A{
+    void show(){
+        System.out.println("Hii I am Jeevan Pareek");
+    }
+    void show1(int a){
+        System.out.println(a);
+    }
+    int show2(){
+        System.out.println("20");
+
+        return 0;
+    }
+    char show3(char a){
+        System.out.println(a);
+
+        return a;
+    }
+    A show4(){
+        System.out.println("Hello");
+
+        return this;
+    }
+}
---
+++ OOPs concepts/Aggregation/Question 3/Q2.java
@@ -0,0 +1,10 @@
+public class Q2 {
+    public static void main(String[] args) {
+        A p = new A();
+        p.show();
+        p.show1(45);
+        p.show2();
+        p.show3('J');
+        p.show4();
+    }
+}
