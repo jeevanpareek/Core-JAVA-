46e907facf418eb5755511501ee22abb80890ff6
jeevan <jeevanpareek1234@gmail.com>
2025-02-21 17:11:54 +0530

    Printing array in reverse direction

---
+++ Array/1-D Array/Q4.java
@@ -0,0 +1,8 @@
+public class Q4 {
+    public static void main(String[] args) {
+        int a[] = {15,23,65,96,87,52,65,41,202,326,458,6985,2568,1257,456};
+        for(int i=a.length-1;i>=0;i--){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
