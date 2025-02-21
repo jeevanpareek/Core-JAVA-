f7eeb563ed54777208dec09b07297624dd37d09d
jeevan <jeevanpareek1234@gmail.com>
2025-02-21 17:00:23 +0530

    Printing multiple arrays of different data types

---
+++ Array/1-D Array/Q2.java
@@ -0,0 +1,20 @@
+public class Q2 {
+    public static void main(String[] args) {
+        char a[] = {'a','e','i','o','u'};
+        String s[] = {"Jeevan","Pareek","Jatin","Sharma","Gaurav"};
+        float f[] = {1.23f,2.36f,3.26f,5.369f,56.235f};
+
+        for (int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+        for (int j=0;j<s.length;j++){
+            System.out.print(s[j]+" ");
+        }
+        System.out.println();
+        for (int k=0;k<f.length;k++){
+            System.out.print(f[k]+" ");
+        }
+        System.out.println();
+    }
+}
