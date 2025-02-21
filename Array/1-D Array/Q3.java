b96d48fecdec9d7b5ff92bf3dd62a7c0486ddef3
jeevan <jeevanpareek1234@gmail.com>
2025-02-21 17:07:53 +0530

    Printing array in forward direction

---
+++ Array/1-D Array/Q3.java
@@ -0,0 +1,8 @@
+public class Q3 {
+    public static void main(String[] args) {
+        int a[] = {15,23,65,96,87,52,65,41,202,326,458,6985,2568,1257,456};
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
