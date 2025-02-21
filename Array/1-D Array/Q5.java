19eeeb4c21d98fa945f531075b0470e1d7eac726
jeevan <jeevanpareek1234@gmail.com>
2025-02-21 17:31:24 +0530

    Printing middle element of an array

---
+++ Array/1-D Array/Q5.java
@@ -0,0 +1,7 @@
+public class Q5 {
+    public static void main(String[] args) {
+        int a[] = {15,23,65,96,87,52,65,41,202,326,458,6985,2568,1257,456};
+            int b = a.length/2;
+            System.out.print(a[b]+" ");
+    }
+}
