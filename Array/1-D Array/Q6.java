d8a1e1f64c6b0eec4e40c48dd57c43b679015863
jeevan <jeevanpareek1234@gmail.com>
2025-02-21 17:38:43 +0530

    Printing first & last element of an array

---
+++ Array/1-D Array/Q6.java
@@ -0,0 +1,9 @@
+public class Q6 {
+    public static void main(String[] args) {
+        int a[] = {15,23,65,96,87,52,65,41,202,326,458,6985,2568,1257,456};
+            int b = a[0];
+            int c = a[a.length-1];
+            System.out.print("The first element of the array is:"+b+" "+ "& the last element of the array is :"+c+" ");
+            
+    }
+}
