e6216359021d27566709f09f5e68df3c80e74baf
jeevan <jeevanpareek1234@gmail.com>
2025-05-10 22:12:27 +0530

    ArrayList Constructor with user-defined initial capacity

---
+++ CollectionFramework/src/arrayList/arrayListConstructors/ArrayListConstructor2.java
@@ -0,0 +1,17 @@
+package arrayList.arrayListConstructors;
+
+import java.util.ArrayList;
+
+public class ArrayListConstructor2 {
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList(5);
+		
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		al.add(50);
+		
+		System.out.println(al);
+	}
+}
