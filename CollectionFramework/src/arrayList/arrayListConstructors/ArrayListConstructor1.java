941152ed386c5e2fa7e0f518553a9fb8b5469010
jeevan <jeevanpareek1234@gmail.com>
2025-05-10 22:11:17 +0530

    ArrayList Constructor with default capacity

---
+++ CollectionFramework/src/arrayList/arrayListConstructors/ArrayListConstructor1.java
@@ -0,0 +1,16 @@
+package arrayList.arrayListConstructors;
+
+import java.util.ArrayList;
+
+public class ArrayListConstructor1 {
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		
+		al.add(25);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		
+		System.out.println(al);
+	}
+}
