e38eb1a2108cabbf2fbd93deb4aa6362d50ba8bf
jeevan <jeevanpareek1234@gmail.com>
2025-05-11 18:56:42 +0530

    clear() method

---
+++ CollectionFramework/src/arrayList/ArrayListMethods/ArrayListMethod10.java
@@ -0,0 +1,26 @@
+package arrayList.ArrayListMethods;
+
+import java.util.ArrayList;
+
+public class ArrayListMethod10 {
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
+		
+		al.add(10);
+		al.add("Jeevan");
+		al.add(20);
+		al.add(10);
+		al.add("Jeevan");
+		al.add(40);
+		al.add(10);
+		al.add("Jeevan");
+		al.add(60);
+		
+		System.out.println("Before " + al);
+		
+		al.clear();
+		
+		System.out.println("After " + al);
+	}
+}
