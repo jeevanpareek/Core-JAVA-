c1c33b9a7b8e34be4ac00f13568ee4bcd2406b0e
jeevan <jeevanpareek1234@gmail.com>
2025-05-11 18:08:23 +0530

    add(int index, E element) method

---
+++ CollectionFramework/src/arrayList/ArrayListMethods/ArrayListMethod2.java
@@ -0,0 +1,18 @@
+package arrayList.ArrayListMethods;
+
+import java.util.ArrayList;
+
+public class ArrayListMethod2 {
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
+		
+		al.add(0, "jeevan");
+		al.add(1, "pareek");
+		al.add(2, 10);
+		al.add(3, 20);
+		
+		System.out.println(al);
+	}
+}
+
