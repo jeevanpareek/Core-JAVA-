d8ee49abd549216a729b9111dd7579e8c9670442
jeevan <jeevanpareek1234@gmail.com>
2025-05-11 18:50:09 +0530

    size() method

---
+++ CollectionFramework/src/arrayList/ArrayListMethods/ArrayListMethod7.java
@@ -0,0 +1,23 @@
+package arrayList.ArrayListMethods;
+
+import java.util.ArrayList;
+
+public class ArrayListMethod7 {
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
+		
+		al.add(10);
+		al.add("Jeevan");
+		al.add(20);
+		al.add(10);
+		al.add("Jeevan");
+		al.add(40);
+		al.add(10);
+		al.add("Jeevan");
+		al.add(60);
+		
+		System.out.println("The size of the give arraylist is : " + al.size());
+		
+	}
+}
