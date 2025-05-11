705a635e1610017581ee33ec6e7afb2fd0ec10fc
jeevan <jeevanpareek1234@gmail.com>
2025-05-11 18:18:00 +0530

    get(int index) method

---
+++ CollectionFramework/src/arrayList/ArrayListMethods/ArrayListMethod3.java
@@ -0,0 +1,19 @@
+package arrayList.ArrayListMethods;
+
+import java.util.ArrayList;
+
+public class ArrayListMethod3 {
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
+		
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		al.add(50);
+		al.add(60);
+		
+		System.out.println(al.get(2));
+	}
+}
